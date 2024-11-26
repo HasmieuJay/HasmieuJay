4a7fec86f85d979b334a51cd719d4b10fdd2baf1
Hasmieu Jay <hasmieu@hasmieus-mbp-2.nau.edu>
2024-11-26 06:42:29 -0700

    Created a little rounding program

---
+++ day1.py
@@ -0,0 +1,15 @@
+def round_func(val): #A float type
+    if val % 2 == 0: #For even numbers
+        remainder = val % 2
+        if remainder >= 0.5:
+            to_add_to_val = 1 - remainder
+            return val + to_add_to_val
+        else:
+            return val - remainder
+    else:
+        remainder = val % 1
+        if remainder >= 0.5:
+            to_add_to_val = 1 - remainder
+            return val + to_add_to_val
+        else:
+            return val - remainder
